73248245267606e0dcef47f8aef251d3e069c272
luisa <luisa.balzus@gmail.com>
2018-10-16 17:42:56 +0200

    complete calculationg mean RT and accuracy for each subject, save values in data frame, do descriptive statistics and bar plots

--- Analysis_evaluative_GNG.R
+++ Analysis_evaluative_GNG.R
@@ -2,8 +2,20 @@
 ##### by Luisa Balzus
 ##### 15.10.2018
 
+
+# install.packages('openxlsx')
+library(openxlsx)
+# install.packages("pastecs")
+library(pastecs)
+# install.packages("ggplot2")
+library(ggplot2)
+
+
 # clear environment
 rm(list=ls())
+
+# create data frame for saving values for each subject in the for-loop
+df4save = NULL
 
 
 ####################   load   #######################################
@@ -11,15 +23,15 @@ rm(list=ls())
 
 # load logfiles, create dataframe 
 
-setwd("P:/LuisaBalzus/1_PhD_Project/6_ModERN_Behavioral_Study/raw_data")                       #path to folder containing the log files (use of forward slashes instead of backward slashes is required); should contain ONLY logfiles 
+setwd("P:/LuisaBalzus/1_PhD_Project/6_ModERN_Behavioral_Study/raw_data")                           #path to folder containing the log files (use of forward slashes instead of backward slashes is required); should contain ONLY logfiles 
 
-logfiles <- list.files("P:/LuisaBalzus/1_PhD_Project/6_ModERN_Behavioral_Study/raw_data")            #lists files in folder
+logfiles <- list.files("P:/LuisaBalzus/1_PhD_Project/6_ModERN_Behavioral_Study/raw_data")          #lists files in folder
 
-for (i in logfiles)                                                                                      # loop reading txt-file by txt-file as table, ommit first 58 lines and added lines after trials; use first line as header
-{ tmp <- (read.table(i, skip = 58, fill = TRUE, header = TRUE, nrows = 516))
+for (subject in logfiles)                                                                          # loop reading txt-file by txt-file as table, ommit first 58 lines and added lines after trials; use first line as header
+{ tmp <- (read.table(subject, skip = 58, fill = TRUE, header = TRUE, nrows = 516))
 #tmp$name <- substr(tmp$name,8,9)                                                                  # change "names" column: have 02 insted of ModERN_02_Flanker
 #tmp <- tmp[!duplicated(tmp$word),]                                                                # delete repeated words
-#if (i=="Luisa_004.txt")
+#if (subject=="Luisa_004.txt")
 #{a1 <- tmp}                                                                                       # at the first iteration, create a1
 #else {a1 <- rbind(a1,tmp)}                                                                        # rbind glues currently loaded file (tmp) to the end of dataframe a1
 
@@ -60,15 +72,14 @@ median_priming_overall <- (median_pos_after_FA + median_neg_after_FH) - (median_
 ####################     exclude   #####################################
 ####################    outliers   #####################################
 
-# assign NA to rt_values deviating more than 3 standard deviations from median
-neg_after_FA$w_rt[which((abs(neg_after_FA$w_rt - median(neg_after_FA$w_rt))/mad(neg_after_FA$w_rt))>3)] <- NA
+# assign NA to rt_values deviating more than 3 median absolute deviations
+
 pos_after_FA$w_rt[which((abs(pos_after_FA$w_rt - median(pos_after_FA$w_rt))/mad(pos_after_FA$w_rt))>3)] <- NA
 neg_after_FH$w_rt[which((abs(neg_after_FH$w_rt - median(neg_after_FH$w_rt))/mad(neg_after_FH$w_rt))>3)] <- NA
 pos_after_FH$w_rt[which((abs(pos_after_FH$w_rt - median(pos_after_FH$w_rt))/mad(pos_after_FH$w_rt))>3)] <- NA
 
 # count number of outliers
 count_outlier <- length(which(is.na(neg_after_FA$w_rt))) + length(which(is.na(pos_after_FA$w_rt))) + length(which(is.na(neg_after_FH$w_rt))) + length(which(is.na(pos_after_FH$w_rt)))
-
 
 # remove the rows with rt_values = NA
 neg_after_FA <- neg_after_FA[!(is.na(neg_after_FA$w_rt)),]
@@ -102,7 +113,7 @@ mean_priming_overall <- (mean_pos_after_FA + mean_neg_after_FH) - (mean_neg_afte
 ####################    count number    ##################################
 ####################     of events      ##################################
 
-# counts number of events, after outlier are removed; if I want to work with median, shift this part before section "exclude outlier"
+# counts number of events, after outliers are removed; if I want to work with median, shift this part before section "exclude outlier"
 
 count_neg_after_FA <- length(neg_after_FA$w_rt)
 count_pos_after_FA <- length(pos_after_FA$w_rt)
@@ -130,19 +141,107 @@ count_all_pos_after_FH <- count_pos_after_FH + count_incorr_pos_after_FH + count
 ####################    calculate    ##################################
 ####################    accuracy     ##################################
 
-percent_correct_neg_after_FA <- 
-accuracy_pos_after_FA <- 
-accuracy_neg_after_FH <- 
-accuracy_pos_after_FH <- 
+percent_correct_neg_after_FA <- count_neg_after_FA / count_all_neg_after_FA * 100 
+percent_correct_pos_after_FA <- count_pos_after_FA / count_all_pos_after_FA * 100  
+percent_correct_neg_after_FH <- count_neg_after_FH / count_all_neg_after_FH * 100  
+percent_correct_pos_after_FH <- count_pos_after_FH / count_all_pos_after_FH * 100  
+
+percent_incorrect_neg_after_FA <- count_incorr_neg_after_FA / count_all_neg_after_FA * 100 
+percent_incorrect_pos_after_FA <- count_incorr_pos_after_FA / count_all_pos_after_FA * 100  
+percent_incorrect_neg_after_FH <- count_incorr_neg_after_FH / count_all_neg_after_FH * 100  
+percent_incorrect_pos_after_FH <- count_incorr_pos_after_FH / count_all_pos_after_FH * 100  
+
+percent_miss_neg_after_FA <- count_miss_neg_after_FA / count_all_neg_after_FA * 100 
+percent_miss_pos_after_FA <- count_miss_pos_after_FA / count_all_pos_after_FA * 100  
+percent_miss_neg_after_FH <- count_miss_neg_after_FH / count_all_neg_after_FH * 100  
+percent_miss_pos_after_FH <- count_miss_pos_after_FH / count_all_pos_after_FH * 100  
 
 
 
 
+####################    save values   ##################################
+####################   in dataframe   ##################################
+
+
+df4save <- rbind(df4save, data.frame(subject,mean_neg_after_FA,mean_pos_after_FA,mean_pos_after_FH,mean_neg_after_FH,sd_neg_after_FA,sd_pos_after_FA,sd_pos_after_FH,sd_neg_after_FH,mean_priming_overall,mean_priming_after_error,mean_priming_after_correct,median_neg_after_FA,median_pos_after_FA,median_pos_after_FH,median_neg_after_FH,median_priming_overall,median_priming_after_error,median_priming_after_correct,count_neg_after_FA,count_pos_after_FA,count_pos_after_FH,count_miss_neg_after_FH,count_outlier,count_incorr_neg_after_FA,count_incorr_pos_after_FA,count_incorr_pos_after_FH,count_incorr_neg_after_FH,percent_correct_neg_after_FA,percent_correct_pos_after_FA,percent_correct_pos_after_FH,percent_correct_neg_after_FH,percent_incorrect_neg_after_FA,percent_incorrect_pos_after_FA,percent_incorrect_pos_after_FH,percent_incorrect_neg_after_FH,percent_miss_neg_after_FA,percent_miss_pos_after_FA,percent_miss_pos_after_FH,percent_miss_neg_after_FH))
 
 }
 
 
 
-#setwd("P:/LuisaBalzus/1_PhD_Project/6_ModERN_Behavioral_Study/Analysis")  # setting a different folder as working directory to prevent saving stuff into the folder containing the logfiles
+####################    save dataframe   ##################################
+####################    as excel file    ##################################
 
-# possibly use command "attach"; then I do not have to indicate columns with $
+
+setwd("P:/LuisaBalzus/1_PhD_Project/6_ModERN_Behavioral_Study/Analysis")                                      # setting a different folder as working directory to prevent saving stuff into the folder containing the logfiles
+
+date_time <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
+
+filename <- paste("SummaryStatisticsEvaluativeGNG_For",length(logfiles),"_subjects_",date_time, ".xlsx", sep = "")
+
+write.xlsx(df4save, file = filename)
+#save(df4save, file = filename)
+
+#current_file <- read.xlsx(filename)                                                                         # read in that file
+
+
+###########################################################################
+###########################################################################
+###########################################################################
+##############################Statistics###################################
+###########################################################################
+###########################################################################
+###########################################################################
+
+
+#####################    descriptive   ####################################
+#####################    statistics    ####################################
+
+# get descriptve statistics
+descriptive_statistics <- stat.desc(df4save,basic=F)
+
+
+#####################      bar     ####################################
+#####################     plots    ####################################
+
+# for RT 
+df4plotRT <- data.frame(
+  response = c("false alarm","false alarm","fast hit","fast hit"),
+  valence = c("neg","pos","neg","pos"),
+  conditions = c("neg_after_FA","pos_after_FA","neg_after_FH","pos_after_FH"),
+  mean = as.numeric(c(descriptive_statistics[2,2],descriptive_statistics[2,3],descriptive_statistics[2,5],descriptive_statistics[2,4])),
+  se = c(70,100,90,80)
+  #for now I had only one subject, so I made se up; later use: se = as.numeric(c(descriptive_statistics[3,2],descriptive_statistics[3,3],descriptive_statistics[3,5],descriptive_statistics[3,4]))
+  )
+
+# plot_rt <- 
+  ggplot(df4plotRT,x = response,y = mean, aes(response, mean, fill = valence))+
+  geom_bar(stat="identity", position=position_dodge()) +                                                       # add bars, based on stats values; dodge to avoid stacked bars
+  geom_errorbar(aes(ymax = mean + se, ymin= mean - se), position = position_dodge(width=0.95), width=0.1) +    # add error bars
+  ggtitle("Mean Response Time") +                                                                              # add title
+  xlab("response") + ylab("Reaction Time in ms") +                                                             # label axes
+  theme(plot.title = element_text(hjust = 0.5)) +                                                              # center title
+  scale_fill_manual(values=c("mediumblue", "limegreen"))                                                       # change bar colors
+  
+  
+# for ACCURACY
+  df4plotACC <- data.frame(
+    response = c("false alarm","false alarm","fast hit","fast hit"),
+    valence = c("neg","pos","neg","pos"),
+    conditions = c("neg_after_FA","pos_after_FA","neg_after_FH","pos_after_FH"),
+    mean = as.numeric(c(descriptive_statistics[2,29],descriptive_statistics[2,30],descriptive_statistics[2,32],descriptive_statistics[2,31])),
+    se = c(5,4,3,2)
+    #for now I had only one subject, so I made se up; later use: se = as.numeric(c(descriptive_statistics[3,29],descriptive_statistics[3,30],descriptive_statistics[3,32],descriptive_statistics[3,31]))
+  )
+  
+  
+  # plot_accuracy <- 
+  ggplot(df4plotACC,x = response,y = mean, aes(response, mean, fill = valence))+
+  geom_bar(stat="identity", position=position_dodge()) +                                                       # add bars, based on stats values; dodge to avoid stacked bars
+  geom_errorbar(aes(ymax = mean + se, ymin= mean - se), position = position_dodge(width=0.95), width=0.1) +    # add error bars
+  ggtitle("Mean Accuracy") +                                                                                   # add title
+  xlab("response") + ylab("Accuracy in %") +                                                                   # label axes
+  theme(plot.title = element_text(hjust = 0.5)) +                                                              # center title
+  scale_fill_manual(values=c("mediumblue", "limegreen"))                                                       # change bar colors
+  
+  
