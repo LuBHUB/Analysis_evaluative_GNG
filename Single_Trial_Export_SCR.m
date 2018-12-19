47758aff5e68c09db48edf1642b6897518bd2fc7
LBHU <43408640+LuBHUB@users.noreply.github.com>
2018-12-19 17:39:36 +0100

    Delete Single_Trial_Export_SCR.m

--- Single_Trial_Export_SCR.m
+++
@@ -1,6 +0,0 @@
-cd('P:\LuisaBalzus\1_PhD_Project\6_ModERN_Behavioral_Study\5_Analyses\ledalab-349')
-Ledalab
-
-%Ledalab('P:\LuisaBalzus\1_PhD_Project\5_ModERN_Vorstudie\7_raw_data\GoNoGo\export_files\SCR\', 'open','visionanalyzer', 'smooth',{'gauss',12}, 'analyze','DDA', 'optimize',2, 'export_era', [1 4 .01 3], 'overview',1, 'zscale',1)
-
-Ledalab('P:\LuisaBalzus\1_PhD_Project\6_ModERN_Behavioral_Study\7_Raw_Data_SCR\export_files\', 'open','visionanalyzer', 'smooth',{'gauss',12}, 'analyze','DDA', 'optimize',2, 'export_era', [1 4 .01 3], 'overview',1, 'zscale',1)
