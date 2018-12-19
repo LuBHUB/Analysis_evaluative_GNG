b7fb2bfcbb67588febba8adc0e257a2e51969726
luisa <luisa.balzus@gmail.com>
2018-12-19 17:47:27 +0100

    SCR export batch

--- Single_Trial_Export_SCR.m
+++ Single_Trial_Export_SCR.m
@@ -1,6 +1,4 @@
 cd('P:\LuisaBalzus\1_PhD_Project\6_ModERN_Behavioral_Study\5_Analyses\ledalab-349')
 Ledalab
+Ledalab('P:\LuisaBalzus\1_PhD_Project\6_ModERN_Behavioral_Study\8_SCR_Export\', 'open','visionanalyzer','downsample',50, 'smooth',{'adapt',8}, 'analyze','DDA', 'optimize',2,'export_era', [1 4 .01 2], 'overview',1)
 
-%Ledalab('P:\LuisaBalzus\1_PhD_Project\5_ModERN_Vorstudie\7_raw_data\GoNoGo\export_files\SCR\', 'open','visionanalyzer', 'smooth',{'gauss',12}, 'analyze','DDA', 'optimize',2, 'export_era', [1 4 .01 3], 'overview',1, 'zscale',1)
-
-Ledalab('P:\LuisaBalzus\1_PhD_Project\6_ModERN_Behavioral_Study\7_Raw_Data_SCR\export_files\', 'open','visionanalyzer', 'smooth',{'gauss',12}, 'analyze','DDA', 'optimize',2, 'export_era', [1 4 .01 3], 'overview',1, 'zscale',1)
