cc4d7e572dcd4975d004791038c72b94407392e7
luisa <luisa.balzus@gmail.com>
2018-12-19 18:23:54 +0100

    test

--- Single_Trial_Export_SCR.m
+++
@@ -1,4 +0,0 @@
-cd('P:\LuisaBalzus\1_PhD_Project\6_ModERN_Behavioral_Study\5_Analyses\ledalab-349')
-Ledalab
-Ledalab('P:\LuisaBalzus\1_PhD_Project\6_ModERN_Behavioral_Study\8_SCR_Export\', 'open','visionanalyzer','downsample',50, 'smooth',{'adapt',8}, 'analyze','DDA', 'optimize',2,'export_era', [1 4 .01 2], 'overview',1)
-
